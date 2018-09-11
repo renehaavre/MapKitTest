8ac4fedbb5afc584288ceeb1d36d5924077fc8c2
Rene Haavre <haavre@gmail.com>
2018-09-12 00:18:54 +0300

    CLLocationManager and authorization setup.

--- MapKitTest/ViewController.swift
+++ MapKitTest/ViewController.swift
@@ -13,9 +13,46 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        checkLocationServices()
+    }
+    
+    func initLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationAuth() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted, .denied:
+            showAlert()
+            break
+        }
+    }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "Warning", message: "Location Services have been disabled.", preferredStyle: UIAlertController.Style.alert)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            initLocationManager()
+            checkLocationAuth()
+        }
+        else {
+            showAlert()
+        }
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
