7c81e12a4fd65ed5009510d8f41602316d972ee9
Rene Haavre <haavre@gmail.com>
2018-09-11 23:49:30 +0300

    Initial commit.

--- MapKitTest/ViewController.swift
+++ MapKitTest/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // TODO
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // TODO
+    }
 
 }
 
